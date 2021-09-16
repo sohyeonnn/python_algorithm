513dc039b3b769bfa7f1755011d5613559b4225c
sohyeonnn <shyeon02060@gmail.com>
2021-09-16 15:49:20 +0900

    문자열

---
+++ BaekJoon/1316.py
@@ -0,0 +1,6 @@
+s = input()
+print(len(s))
+
+import sys
+s1= sys.stdin.readline()
+print(len(s1)-1)
--- BaekJoon/2941.py
+++ BaekJoon/2941.py
@@ -2,12 +2,14 @@
 '''
 **replace
 문자열.replace(검색문자, '치환문자')
+
+
 '''
 
 
 import sys
-#s = sys.stdin.readline()    #입력시 문자열1개를 추가로 인식함 -> 왜....?
-s = input()
+s = sys.stdin.readline()    #입력시 문자열1개를 추가로 인식함 -> 왜...? => 문자열에 개행문자(\n ) 을 기본으로 추가인식하기 때문에
+#s = input()
 
 alpha_list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
