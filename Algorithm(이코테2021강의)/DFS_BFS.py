366984e1605875f704d713cb98505d07ce7ea47b
sohyeonnn <shyeon02060@gmail.com>
2021-10-01 14:19:03 +0900

    이코테

---
+++ Algorithm(이코테2021강의)/DFS_BFS.py
@@ -0,0 +1,17 @@
+#DFS/BFS
+'''
+#탐색(search);
+- 많은 양의 데이터 중에서 원하는 데이터를 찾는 과정을 말한다.
+
+
+#스택(stack) 자료구조
+- FILO ; ex.박스쌓기
+- list로 간편하게 구현 가능(삽입: append, 삭제: pop)
+- 시간복잡도; O(1)
+
+
+#큐(queue) 자료구조
+- FIFO ; ex. 입구랑 출구가 모두 뚫려있는 터널
+- from collections import deque(삽입: append, 삭제: popleft)
+- 시간복잡도; O(1)
+'''
