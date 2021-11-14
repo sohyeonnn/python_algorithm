22d8365f879d3ef40a3c8ef4000c3455a43a90c6
sohyeonnn <shyeon02060@gmail.com>
2021-11-14 23:31:16 +0900

    막대기_비트마스킹(추가)

--- BaekJoon/1094.py
+++ BaekJoon/1094.py
@@ -1,9 +1,8 @@
 #막대기_비트마스크(실4)
-'''
-비트마스킹으로 안푼듯...?
-'''
-import sys
 
+import sys
+'''
+#비트마스킹x 풀이
 n = int(sys.stdin.readline())
 stick = [64, 32, 16, 8, 4, 2, 1]
 result = 0
@@ -13,3 +12,9 @@ for i in stick:
         n -= i
         result += 1
 print(result)
+'''
+
+#비트마스킹/1이 몇개인지 세는게 더 효율적인 풀이
+n = int(sys.stdin.readline())
+
+print(bin(n).count('1'))
