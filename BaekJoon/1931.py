9e97f57447fbbabd0d6398efa6a33eae2cbf713c
sohyeonnn <shyeon02060@gmail.com>
2021-11-26 23:37:46 +0900

    회의실배정_그리디_다른풀이

--- BaekJoon/1931.py
+++ BaekJoon/1931.py
@@ -8,7 +8,10 @@
 
 ***(1step)끝나는 시간의 오름차순 ->(2step)시작하는 시간의 오름차순으로 정렬해줘야함
 '''
+
+#예전풀이
 import sys
+'''
 n = int(sys.stdin.readline().rstrip())
 
 time = []
@@ -21,6 +24,8 @@ time.sort()
 time.sort(key=lambda x:x[1])
 
 '''
+
+'''
 for i in range(N):
     s, e = map(int, sys.stdin.readline().split())
     time[i][0] = s
@@ -28,6 +33,33 @@ for i in range(N):
 
 이렇게도 가능
 '''
+
+'''
+room = 1
+end_time = time[0][1]
+
+#range(1,n)
+for i in range(1, n):
+    if time[i][0] >= end_time:
+        room += 1
+        end_time = time[i][1]
+print(room)
+'''
+
+
+#다시플기
+#끝나는시간의 내림차순으로 정렬하면 되지않을까?
+n = int(sys.stdin.readline().rstrip())
+
+time = []
+
+for i in range(n):
+    s, e = map(int, sys.stdin.readline().split())
+    time.append((s, e))
+
+#2번째 인자(key)에 대해 reverse sort
+time.sort(key=lambda x:x[1], reverse=True)
+
 room = 1
 end_time = time[0][1]
 
