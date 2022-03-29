c9cec8af16dc3533a0880a087337356a97dda2ad
sohyeonnn <shyeon02060@gmail.com>
2022-03-29 15:40:17 +0900

    DFS와 BFS_(re)

--- BaekJoon/1260.py
+++ BaekJoon/1260.py
@@ -28,6 +28,7 @@ graph[m2].append(m1)
 해결2. dfs는 reversed(sorted())해서 처리
 '''
 
+'''
 import sys
 from collections import deque
 
@@ -70,6 +71,7 @@ def bfs(graph, v):
 
 print(' '.join(list(map(str, dfs(graph, v)))))
 print(' '.join(list(map(str, bfs(graph, v)))))
+'''
 
 '''
 def bfs(graph, startV):
@@ -93,3 +95,51 @@ def bfs(graph, startV):
 
 print(bfs(graph, startV))
 '''
+
+import sys
+from collections import deque
+
+n, m, start = map(int, sys.stdin.readline().split())    #정점개수/간선개수/시작정점번호
+graph = [[] for i in range(n+1)]
+
+
+for i in range(m):
+    n1, n2 = map(int, sys.stdin.readline().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)    #인접리스트로 구현
+
+#print(graph)    # [[], [2, 3, 4], [1, 3], [1, 2, 4], [1, 3]]
+
+
+def dfs(graph, start, visited):
+    visited[start] = True
+    print(start, end=' ')
+
+    graph[start].sort()     #**문제조건; 방문할 수 있는 정점이 여러개인 경우에는 정점 번호가 작은것을 먼저 방문
+    for node in graph[start]:
+
+        if not visited[node]:
+            dfs(graph, node, visited)
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        now = queue.popleft()
+        print(now, end=' ')
+
+        graph[now].sort()       #**문제조건; 방문할 수 있는 정점이 여러개인 경우에는 정점 번호가 작은것을 먼저 방문
+        for i in graph[now]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+visited = [False] * (n+1)
+
+dfs(graph, start, visited)
+visited = [False] * (n+1)
+print()
+bfs(graph, start, visited)
