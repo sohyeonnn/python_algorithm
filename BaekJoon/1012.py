f87fb1d7c95bafb6c827fe0c9916b0876c7c40b9
sohyeonnn <shyeon02060@gmail.com>
2022-04-15 17:03:30 +0900

    유기농 배추_dfs,bfs(추가)

--- BaekJoon/1012.py
+++ BaekJoon/1012.py
@@ -5,11 +5,10 @@
 
 배추들이 몇 군데에 퍼져있는지 조사하면 총 몇마리의 지렁이가 필요한지 알 수 있다.
 -> 얼음얼리는 문제 유형
--> connected component
+-> connected component(dfs)
 
 1. graph=[]를 만들고 배추의 위치를 좌표로 입력받음
 배추있음이니까 1롤 입력받고 없는곳은 0으로 입력받음
-2.
 '''
 import sys
 sys.setrecursionlimit(10**7)    #재귀호출 최대 횟수를 늘려준다, 파이썬에서 기본 재귀깊이는 1000이기 때문에 이것을 늘려준것
@@ -20,7 +19,8 @@ input = sys.stdin.readline
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
+'''
+#dfs 풀이
 def dfs(x, y):
 
     for i in range(4):
@@ -30,6 +30,23 @@ def dfs(x, y):
         if 0 <= nx < n and 0 <= ny < m and baechubat[nx][ny] == 1:  #범위에 속하면서 배추가 있느 경우
             baechubat[nx][ny] = -1  #방문처리
             dfs(nx, ny)
+'''
+
+#bfs 풀이
+def bfs(x, y):
+    queue = deque()
+    queue.append([x, y])
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < n and 0 <= ny < m and baechubat[nx][ny] == 1:
+                baechubat[nx][ny] = -1
+                queue.append([nx, ny])
 
 
 T = int(input().rstrip())   #테스트케이스 수
@@ -46,7 +63,8 @@ for _ in range(T):  #T만큼 반복
     for i in range(n):
         for j in range(m):
             if baechubat[i][j] == 1:
-                dfs(i, j)
+                #dfs(i, j)
+                bfs(i, j)
                 count += 1
 
     print(count)
